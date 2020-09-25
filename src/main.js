3bf2f88f5f50e087d018db296e7a0b1cd4e28daa
wuchao <2425662598@qq.com>
2020-09-25 16:59:50 +0800

    [feature]添加全局守卫

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import { from } from "core-js/fn/array";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-// router.beforeEach((to, from, next) => {
-//   console.log(to)
-// })
+router.beforeEach((to, from, next) => {
+  console.log(to)
+  next()
+})
 Vue.config.productionTip = false;
  
 new Vue({
